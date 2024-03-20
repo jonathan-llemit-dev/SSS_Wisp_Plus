131d7737b72852b000cffbe0ff4c5727cf680d42
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-03-20 10:46:33 +0800

    inputs modiifed

--- application/views/wisp_plus_form.php
+++ application/views/wisp_plus_form.php
@@ -1,73 +1,96 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit ('No direct script access allowed');
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	<script defer src="assets/js/bootstrap.bundle.min.js"></script>
-	<title>Welcome to CodeIgniter</title>
-</head>
-<body>
 
-<div class="card">
-  <div class="card-body">
-    <form class="row g-3">
-      <div class="col-md-6">
-        <label for="inputEmail4" class="form-label">Email</label>
-        <input type="email" class="form-control" id="inputEmail4">
-      </div>
-      <div class="col-md-6">
-        <label for="inputPassword4" class="form-label">Password</label>
-        <input type="password" class="form-control" id="inputPassword4">
-      </div>
-      <div class="col-12">
-        <label for="inputAddress" class="form-label">Address</label>
-        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-      </div>
-      <div class="col-12">
-        <label for="inputAddress2" class="form-label">Address 2</label>
-        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-      </div>
-      <div class="col-md-6">
-        <label for="inputCity" class="form-label">City</label>
-        <input type="text" class="form-control" id="inputCity">
-      </div>
-      <div class="col-md-4">
-        <label for="inputState" class="form-label">State</label>
-        <select id="inputState" class="form-select">
-          <option selected>Choose...</option>
-          <option>...</option>
-        </select>
-      </div>
-      <div class="col-md-2">
-        <label for="inputZip" class="form-label">Zip</label>
-        <input type="text" class="form-control" id="inputZip">
-      </div>
-      <div class="col-12">
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" id="gridCheck">
-          <label class="form-check-label" for="gridCheck">
-            Check me out
-          </label>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <script defer src="assets/js/bootstrap.bundle.min.js"></script>
+  <title>SSS Wisp Plus</title>
+</head>
+
+<body>
+  <div class="container-fluid">
+    <div class="row justify-content-center">
+
+      <!-- Navigation-->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4" id="mainNav">
+        <div class="container px-4">
+          <a class="navbar-brand" href="#page-top">SSS Wisp-plus Form</a>
+        </div>
+      </nav>
+
+      <div class="card bg-light border-dark-subtle" style="max-width: 54rem;">
+        <div class="card-header bg-light">
+          Generate Payment Reference Number (PRN)*
+        </div>
+        <div class="card-body">
+          <form class="row g-3">
+            <div class="col-md-6">
+              <label for="sssnumber" class="form-label">SSS Number*</label>
+              <input type="text" class="form-control" id="sssnumber">
+            </div>
+            <div class="col-md-6">
+              <label for="membertype" class="form-label">Member Type*</label>
+              <select class="form-select" id="membertype" aria-label="Default select example">
+                <option value="1">W - Employed</option>
+                <option value="1">S - Self Employed</option>
+                <option value="1">V - Voluntary Member</option>
+                <option value="1">N - Non-Working Spouse</option>
+                <option value="1">O - Overseas Filipino Worker</option>
+              </select>
+            </div>
+            <div class="col-12">
+              <label for="inputAddress" class="form-label">Address</label>
+              <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            </div>
+            <div class="col-12">
+              <label for="inputAddress2" class="form-label">Address 2</label>
+              <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="col-md-6">
+              <label for="inputCity" class="form-label">City</label>
+              <input type="text" class="form-control" id="inputCity">
+            </div>
+            <div class="col-md-4">
+              <label for="inputState" class="form-label">State</label>
+              <select id="inputState" class="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div class="col-md-2">
+              <label for="inputZip" class="form-label">Zip</label>
+              <input type="text" class="form-control" id="inputZip">
+            </div>
+            <div class="col-12">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="gridCheck">
+                <label class="form-check-label" for="gridCheck">
+                  Check me out
+                </label>
+              </div>
+            </div>
+            <div class="col-12">
+              <button type="submit" class="btn btn-primary">Sign in</button>
+            </div>
+          </form>
         </div>
       </div>
-      <div class="col-12">
-        <button type="submit" class="btn btn-primary">Sign in</button>
-      </div>
-    </form>
+
+      <footer class="pt-2 mt-4">
+        <ul class="nav justify-content-center border-bottom pb-1 mb-1">
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Privacy Policy</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">NPC Seal of Registration</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Terms & Conditions</a></li>
+        </ul>
+        <p class="text-center text-body-secondary">Copyright &copy; 2024 USSC. All rights reserved.</p>
+      </footer>
+
+    </div>
   </div>
-</div>
-
-<footer class="pt-2 mt-2 fixed-bottom">
-  <ul class="nav justify-content-center border-bottom pb-1 mb-1">
-    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Privacy Policy</a></li>
-    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">NPC Seal of Registration</a></li>
-    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Terms & Conditions</a></li>
-  </ul>
-  <p class="text-center text-body-secondary">Copyright &copy; 2024 USSC. All rights reserved.</p>
-</footer>
-
 </body>
+
 </html>
